df27f87ab1cdbc3145399b16753ceca7a85956f6
zua <killthelast01@gmail.com>
2023-10-02 18:48:31 +0100

    fix extension

--- stage-five/task-1/helpmeout-extension-fin/background.js
+++ stage-five/task-1/helpmeout-extension-fin/background.js
@@ -1,9 +1,11 @@
+//chrome
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
     chrome.scripting
       .executeScript({
         target: { tabId },
-        files: ['./contentScript.js'],
+        files: ['./content.js'],
       })
       .then(() => {
         console.log('we have injected the content script');
